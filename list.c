f7a7d2bb1e6692d30fe2b5b99fec79444753d329
rajat <rajat4113@gmail.com>
2020-04-30 19:09:34 +0530

    added add_to_end and display method in list.c
    added methods in numbers.c to take input and perform
    insert operations on list

--- list.c
+++ list.c
@@ -1,38 +1,63 @@
-#include "list.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "list.h"
 
 Node *create_node(value)
 {
   Node *node = malloc(sizeof(Node));
-  if (node != NULL)
-  {
-    node->value = value;
-    node->next = NULL;
-  }
+  if (node == NULL)
+    return node;
+  node->value = value;
+  node->next = NULL;
   return node;
 }
 
 List_ptr create_list(void)
 {
   List_ptr list = malloc(sizeof(List));
-  if (list != NULL)
-  {
-    list->head = NULL;
-    list->last = NULL;
-    list->count = 0;
-  }
+  if (list == NULL)
+    return list;
+  list->head = NULL;
+  list->last = NULL;
+  list->count = 0;
   return list;
 }
 
 Status add_to_start(List_ptr list, int value)
 {
   Node *element = create_node(value);
-  if (element != NULL)
+  if (element == NULL)
+    return Failure;
+  element->next = list->head;
+  list->head = element;
+  list->count += 1;
+  return Success;
+}
+
+Status add_to_end(List_ptr list, int value)
+{
+  Node *element = create_node(value);
+  if (element == NULL)
+    return Failure;
+  if (list->head == NULL)
   {
-    element->next = list->head;
     list->head = element;
-    list->count += 1;
-    return Success;
   }
-  return Failure;
+  else
+  {
+    list->last->next = element;
+  }
+  list->last = element;
+  list->count += 1;
+  return Success;
+}
+
+void display(List_ptr list)
+{
+  Node *p_walk = list->head;
+  while (p_walk != NULL)
+  {
+    printf("%d\n", p_walk->value);
+    p_walk = p_walk->next;
+  }
 }
--- numbers.c
+++ numbers.c
@@ -1,7 +1,66 @@
 #include <stdio.h>
 #include "list.h"
 
+int get_input(char *input_text)
+{
+  int number;
+  printf("%s", input_text);
+  scanf("%d", &number);
+  return number;
+}
+
+char ask_option()
+{
+  char option;
+  printf("(a) add a number to the end of the list\n");
+  printf("(b) add a number to the start of the list\n");
+  printf("(l) display the list of numbers\n");
+  printf("(m) exit\n");
+  printf("\nPlease enter the alphabet of the operation you would like to perform\n");
+  scanf("%c", &option);
+  return option;
+}
+
+void perform_operation(List_ptr list, char option)
+{
+  int number;
+  switch (option)
+  {
+  case 'a':
+    number = get_input("Enter a number : ");
+    Status status = add_to_end(list, number);
+    if (status == Success)
+    {
+      printf("%d is added to the end of the list\n\n", number);
+    }
+    break;
+  case 'b':
+    number = get_input("Enter a number : ");
+    Status status = add_to_start(list, number);
+    if (status == Success)
+    {
+      printf("%d is added to the start of the list\n\n", number);
+    }
+    break;
+  case 'l':
+    display(list);
+    printf("\n");
+    break;
+  default:
+    printf("wrong option!\n");
+    break;
+  }
+}
+
 int main(void)
 {
-  return 0;
+  char selected_option = ask_option();
+  List_ptr list = create_list();
+
+  while (selected_option != 'm')
+  {
+    perform_operation(list, selected_option);
+    fflush(stdin);
+    selected_option = ask_option();
+  }
 }
