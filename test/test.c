13683ec08c9caa8ee56c7cbed96c096bad32d355
rajat <rajat4113@gmail.com>
2020-05-06 15:46:49 +0530

    added test for add_to_start function

--- test/test.c
+++ test/test.c
@@ -13,9 +13,35 @@ test_status assert_add_to_end_in_empty_list()
   return Fail;
 }
 
+test_status assert_add_to_end()
+{
+  List_ptr list = create_list();
+  add_to_end(list, 10);
+  Status status = add_to_end(list, 20);
+  if (list->last->value == 20 && status)
+  {
+    return Pass;
+  }
+  return Fail;
+}
+
+test_status assert_add_to_start_in_empty_list()
+{
+  List_ptr list = create_list();
+  Status status = add_to_start(list, 10);
+  if (list->head->value == 10 && status)
+  {
+    return Pass;
+  }
+  return Fail;
+}
+
 int main(void)
 {
   describe("add_to_end()");
   it("should add to the end of empty list", assert_add_to_end_in_empty_list());
+  it("should add to the end of list", assert_add_to_end());
+  describe("add to start of the list");
+  it("should add to the start of empty list", assert_add_to_start_in_empty_list());
   return 0;
 }
