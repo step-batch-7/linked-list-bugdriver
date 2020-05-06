b6a4791e69c2584d4289a9e1a589953407b920fe
rajat <rajat4113@gmail.com>
2020-05-06 15:51:48 +0530

    added test for insert_at function

--- test/test.c
+++ test/test.c
@@ -36,6 +36,42 @@ test_status assert_add_to_start_in_empty_list()
   return Fail;
 }
 
+test_status assert_add_to_start()
+{
+  List_ptr list = create_list();
+  add_to_end(list, 10);
+  Status status = add_to_start(list, 20);
+  if (list->head->value == 20 && list->head != list->last && status)
+  {
+    return Pass;
+  }
+  return Fail;
+}
+
+test_status assert_insert_at()
+{
+  List_ptr list = create_list();
+  add_to_end(list, 10);
+  Status s = insert_at(list, 20, 0);
+  if (list->head->value == 20 && s)
+  {
+    return Pass;
+  }
+  return Fail;
+}
+
+test_status assert_insert_at_wrong_position()
+{
+  List_ptr list = create_list();
+  Status s1 = insert_at(list, 10, 2);
+  Status s2 = insert_at(list, 10, -2);
+  if (list->count == 0 && !s1 && !s2)
+  {
+    return Pass;
+  }
+  return Fail;
+}
+
 int main(void)
 {
   describe("add_to_end()");
@@ -43,5 +79,9 @@ int main(void)
   it("should add to the end of list", assert_add_to_end());
   describe("add to start of the list");
   it("should add to the start of empty list", assert_add_to_start_in_empty_list());
+  it("should add to the start of list", assert_add_to_start());
+  describe("insert at given position in the list");
+  it("should insert_at given position in list", assert_insert_at());
+  it("should not insert at invalid position", assert_insert_at_wrong_position());
   return 0;
 }
